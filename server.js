68fcadfb80c41e10e419fd3945978b201d86bfb7
Hani <heenoow@gmail.com>
2022-02-20 19:58:59 +0200

    added web sockets

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when client connects
+io.on('connection', (socket) => {
+  //   Welcome current user
+  socket.emit('message', 'Welcome to chatCord!');
+
+  //   To clients connecting
+  socket.broadcast.emit('message', 'A user has joinded the chat');
+
+  socket.on('disconnect', () => {
+    //   To general clients
+    io.emit('message', 'A user has left the chat');
+  });
+});
+
+let PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`SERVER RUNNING ON http://localhost:${PORT}`));
